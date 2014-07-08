3716dc58eab8368c3229835e5816accc7c3d931a
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2014-07-08 14:21:16 +0530

    still error

--- db/migrate/20140708064420_add_organization_id_to_projectdetails.rb
+++
@@ -1,5 +0,0 @@
-class AddOrganizationIdToProjectdetails < ActiveRecord::Migration
-  def change
-    add_column :projectdetails, :organization_id, :integer
-  end
-end
--- db/migrate/20140708065251_add_projectdetail_id_to_organizations.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectdetailIdToOrganizations < ActiveRecord::Migration
-  def change
-    add_column :organizations, :projectdetail_id, :integer
-  end
-end
